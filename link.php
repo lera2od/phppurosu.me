5ad384b104cbaa6eddada4cd207ccc5a59a5af11
lera2od <119118688+lera2od@users.noreply.github.com>
2024-01-09 22:52:15 +0300

    sa

--- link.php
+++ link.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 $links = json_decode(file_get_contents("private/links.json"), true);
 $uri = $_SERVER["REQUEST_URI"];
-if($uri == "/"){
+if ($uri == "/") {
     header("Location: /index.html");
     die();
 }
@@ -14,7 +14,7 @@ foreach ($links as $linkk) {
         $link = $linkk;
         break;
     }
-    if(isset($linkk["aliases"])){
+    if (isset($linkk["aliases"])) {
         foreach ($linkk["aliases"] as $alias) {
             if ($alias == $uri) {
                 $link = $linkk;
@@ -24,49 +24,13 @@ foreach ($links as $linkk) {
     }
 }
 
-if(!isset($link)){
+if (!isset($link)) {
     echo "404 Not Found";
     http_response_code(404);
     header("Refresh: 3; url=/");
     die();
 }
+
+header("Location: " . $link["to"]);
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PhpPurosu.me</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <hr>
-    <h1>PHPPUROSU</h1>
-    <hr>
-    <h2 style="text-align: center;">REDIRECTING TO</h2>
-    <h3 style="text-align: center;"><?= $link["to"]?></h3>
-    <script>
-        setTimeout(() => {
-            window.location.href = "<?= $link["to"]?>";
-        }, 1500);
-
-        let span = document.querySelector("span");
-        let i = 0;
-        setInterval(() => {
-            if (i == 0) {
-                span.innerHTML = ".";
-                i++;
-            } else if (i == 1) {
-                span.innerHTML = "..";
-                i++;
-            } else if (i == 2) {
-                span.innerHTML = "...";
-                i = 0;
-            }
-        }, 500);
-    </script>
-</body>
-
-</html>
